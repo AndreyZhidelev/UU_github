16ccebabcc885d838aa36556f7a326758180cd8b
Andrey <trahimonada@gmail.com>
2024-12-10 16:58:16 +0400

    10/12

--- Модуль 3/Задание_3_03.py
+++ Модуль 3/Задание_3_03.py
@@ -1,1 +1,22 @@
-# 1.Функция с параметрами по умолчанию:
+def print_params(a = 1, b = 'строка', c = True):
+    print(a , b , c)
+
+print('1. Функция с параметрами по умолчанию:')
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+print_params()
+print_params(25)
+print_params(a = 11, c = 1)
+print()
+
+
+print('2. Распаковка параметров:')
+values_list = ['Пятерочка', 2.55, True]
+values_dict = {'a' : 2, 'b' : 'Стол', 'c' : False}
+print_params(*values_list)
+print_params(**values_dict)
+print()
+
+print('3.Распаковка + отдельные параметры:')
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
